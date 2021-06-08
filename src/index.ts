1e176fd879ac385b6c7b811e7365d27f6724196f
Bruno Leonardo Michels <brunolm7@gmail.com>
2021-06-08 15:40:31 -0300

    Preserve old claims

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,10 @@ const start = async () => {
   console.log('options', options)
   console.log('user', user)
 
-  await admin.auth().setCustomUserClaims(user.uid, { [options.claimName]: options.claimValue })
+  await admin.auth().setCustomUserClaims(user.uid, { ...user.customClaims, [options.claimName]: options.claimValue })
+
+  const userAfterUpdate = await admin.auth().getUserByEmail(options.email)
+  console.log('after claims', userAfterUpdate.customClaims)
 
   console.log('done!')
 }
